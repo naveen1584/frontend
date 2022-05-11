73c1ffbe339ea14dba0cc6b13926162ebd68616c
David North <david.north@oc.edu>
2022-05-11 10:57:30 -0500

    Update : Remove all Bootstrap references

--- src/components/AddTutorial.vue
+++ src/components/AddTutorial.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="submit-form">
-    <div v-if="!submitted">
-      <div class="form-group">
-        <label for="title">Title</label>
+  <div>
+    <div v-if="!submitted" >
+      <div>
+        <label >Title</label>
         <input
           type="text"
-          class="form-control"
           id="title"
           required
           v-model="tutorial.title"
           name="title"
         />
       </div>
-      <div class="form-group">
-        <label for="description">Description</label>
+      <div>
+        <label >Description</label>
         <input
-          class="form-control"
+          
           id="description"
           required
           v-model="tutorial.description"
           name="description"
         />
       </div>
-      <button @click="saveTutorial" class="btn btn-success">Submit</button>
+      <button @click="saveTutorial" >Submit</button>
     </div>
     <div v-else>
       <h4>You submitted successfully!</h4>
-      <button class="btn btn-success" @click="newTutorial">Add</button>
+      <button  @click="newTutorial">Add</button>
     </div>
   </div>
 </template>
